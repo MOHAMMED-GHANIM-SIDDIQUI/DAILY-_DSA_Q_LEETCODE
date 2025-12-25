dd72b9363cee0c2093df80e53450f709ef4456cd
MOHAMMED GHANIM SIDDIQUI <141924512+MOHAMMED-GHANIM-SIDDIQUI@users.noreply.github.com>
2025-12-25 07:31:13 +0530

    Create 3075-LEETCODE..py

---
+++ 3075-LEETCODE..py
@@ -0,0 +1,15 @@
+class Solution:
+  def maximumHappinessSum(self, happiness: list[int], k: int) -> int:
+        happiness.sort(reverse = True)
+
+        res = 0
+
+        for i in range(k):
+            gain = happiness[i] - i
+
+            if gain <= 0:
+                return res
+            
+            res += gain
+        
+        return res
