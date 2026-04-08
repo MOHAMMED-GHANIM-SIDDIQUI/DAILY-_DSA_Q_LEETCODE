e23881ac2e17fab4d6f52fa576d3e90f73a8c01d
MOHAMMED GHANIM SIDDIQUI <141924512+MOHAMMED-GHANIM-SIDDIQUI@users.noreply.github.com>
2026-04-08 16:13:53 +0530

    Create 3653-LEETCODE.py

---
+++ 3653-LEETCODE.py
@@ -0,0 +1,9 @@
+class Solution:
+    def xorAfterQueries(self, nums: List[int], queries: List[List[int]]) -> int:
+      for l , r , k , v in queries:
+        for i in range(l,r+1,k):
+          nums[i]=(nums[i] * v) % (10**9 + 7)
+      ans = 0
+      for val in nums:
+        ans ^= val
+      return ans 
