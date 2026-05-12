34bc12c5bc9dcc6ef818bded1471c7d639e40f27
MOHAMMED GHANIM SIDDIQUI <141924512+MOHAMMED-GHANIM-SIDDIQUI@users.noreply.github.com>
2026-05-12 15:42:13 +0530

    Update 1665-LEETCODE.py

--- 1665-LEETCODE.py
+++ 1665-LEETCODE.py
@@ -31,4 +31,26 @@ class Solution:
         return ans
 
 
+class Solution:
+    def minimumEffort(self, tasks: List[List[int]]) -> int:
+        
+        # Sort by (minimum - actual) descending
+        tasks.sort(key=lambda x: (x[1] - x[0]), reverse=True)
+
+        energy = 0
+        current = 0
+
+        for actual, minimum in tasks:
+            
+            # If current energy is less than required minimum
+            if current < minimum:
+                energy += (minimum - current)
+                current = minimum
+
+            # Do the task
+            current -= actual
+
+        return energy
+
+
 
